780de4a229a9cd6bbbe7d9279ed553ec28456a72
Horacio Bertorello <svankie@gmail.com>
2013-11-28 13:37:47 -0300

    Refactors tests so that they usa Shoulda.

--- test/test_notification_parser.rb
+++ test/test_notification_parser.rb
@@ -8,35 +8,57 @@ class TestDineroMailIpnNotificationParser < Test::Unit::TestCase
     @bad_xml_file = fixture_file("malformed-ipn-v2-notification.xml")
   end
 
-  def test_invalid_initialization
-    assert_raise ArgumentError do
-      DineroMailIpn::NotificationParser.new
+  context "initialization" do
+    should "fail when no XML is provided" do
+      assert_raise ArgumentError do
+        DineroMailIpn::NotificationParser.new
+      end
+    end
+
+    should "fail when an invalid XML is provided" do
+      notification_parser = DineroMailIpn::NotificationParser.new(@bad_xml_file)
+
+      assert !notification_parser.valid?
+    end
+
+    should "pass when a valid XML is provided" do
+      notification_parser = DineroMailIpn::NotificationParser.new(@xml_file)
+
+      assert notification_parser.doc.kind_of? Nokogiri::XML::Document
+      assert notification_parser.valid?
     end
   end
 
-  def test_valid_initialization
-    notification_parser = DineroMailIpn::NotificationParser.new(@xml_file)
-    assert notification_parser.doc.kind_of? Nokogiri::XML::Document
-    assert notification_parser.valid?
-  end
+  context "retrieving notified operations" do
+    context "from a valid XML notification" do
+      should "retrieve all valid notified operations" do
+        notification_parser = DineroMailIpn::NotificationParser.new(@xml_file)
 
-  def test_retrieve_valid_operations
-    notification_parser = DineroMailIpn::NotificationParser.new(@xml_file)
-    notification = notification_parser.parse
-    operations = notification.operations
-    assert operations.is_a? Array
-    assert_equal operations.size, 2
-  end
+        notification = notification_parser.parse
 
-  def test_invalid_xml_file
-    notification_parser = DineroMailIpn::NotificationParser.new(@bad_xml_file)
+        operations = notification.operations
 
-    assert !notification_parser.valid?
+        assert operations.is_a? Array
+        assert_equal operations.size, 2
+      end
+    end
 
-    notification = notification_parser.parse
-    assert notification.nil?
+    context "from an invalid XML notification" do
+      should "return nothing if XML is invalid" do
+        notification_parser = DineroMailIpn::NotificationParser.new(@bad_xml_file)
+        notification = notification_parser.parse
 
-    error = notification_parser.validate.map(&:message).first
-    assert error =~ /No matching global declaration available for the validation root/
+        assert_nil notification
+      end
+
+      should "return a list of errors for that invalid XML" do
+        notification_parser = DineroMailIpn::NotificationParser.new(@bad_xml_file)
+
+        errors = notification_parser.validate
+        error_message = errors.map(&:message).first
+
+        assert error_message =~ /No matching global declaration available for the validation root/
+      end
+    end
   end
 end
